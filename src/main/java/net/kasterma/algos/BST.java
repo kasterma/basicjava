343a400b11b8a13b5749f7bf1c1fad887b65750a
Bart Kastermans <kasterma@kasterma.net>
2018-04-13 08:17:16 +0200

    BST basic insert and find

--- src/main/java/net/kasterma/algos/BST.java
+++ src/main/java/net/kasterma/algos/BST.java
@@ -7,23 +7,32 @@ import java.util.Optional;
 /**
  * Binary search tree
  */
-class BST<Key, Value> {
+class BST<Key extends Comparable<Key>, Value> {
     private Node _root;
 
     private BST() {
         _root = null;
     }
 
-    static <Key, Value> BST<Key, Value> empty() {
+    static <Key extends Comparable<Key>, Value> BST<Key, Value> empty() {
         return new BST<Key,Value>();
     }
 
     void insert(Key key, Value value) {
+        if (_root == null) {
+            _root = new Node(key, value);
+        } else {
+            _root.insert(key, value);
+        }
 
     }
 
     Optional<Value> find(Key key) {
-        return Optional.empty();
+        if (_root == null) {
+            return Optional.empty();
+        } else {
+            return _root.find(key);
+        }
     }
 
     public String toString() {
@@ -46,6 +55,44 @@ class BST<Key, Value> {
             _value = value;
             _left = null;
             _right = null;
+        }
+
+        void insert(Key key, Value value) {
+            int cmp = key.compareTo(_key);
+            if (cmp < 0) {
+                if (_left == null) {
+                    _left = new Node(key, value);
+                } else {
+                    _left.insert(key, value);
+                }
+            } else if (cmp > 0) {
+                if (_right == null) {
+                    _right = new Node(key, value);
+                } else {
+                    _right.insert(key, value);
+                }
+            } else { // cmp == 0
+                _value = value;
+            }
+        }
+
+        Optional<Value> find(Key key) {
+            int cmp = key.compareTo(_key);
+            if (cmp < 0) {
+                if (_left == null) {
+                    return Optional.empty();
+                } else {
+                    return _left.find(key);
+                }
+            } else if (cmp > 0) {
+                if (_right == null) {
+                    return Optional.empty();
+                } else {
+                    return _right.find(key);
+                }
+            } else { // cmp == 0
+                return Optional.of(_value);
+            }
         }
     }
 }
--- src/test/java/net/kasterma/algos/BSTTest.java
+++ src/test/java/net/kasterma/algos/BSTTest.java
@@ -8,6 +8,10 @@ class BSTTest {
     @Test
     void test1() {
         BST<Integer, Integer> bst = BST.empty();
+        bst.insert(0, 0);
+        bst.insert(1, 1);
+        assertEquals(0, (int) bst.find(0).get());
+        assertEquals(1, (int) bst.find(1).get());
     }
 
 }
